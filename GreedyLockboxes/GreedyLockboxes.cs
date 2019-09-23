2e71c9e6c7f1729d2e61e51f5be9bcec1de3186b
harbingerofme <harbingerofme@gmail.com>
2019-09-23 08:03:53 +0200

    Greedy Lockboxes
    This mod will not see thunderstore release.

---
+++ GreedyLockboxes/GreedyLockboxes.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+
+/*
+    Code By Guido "Harb". 
+     */
+
+namespace GreedyLockbox
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.harbingerofme.greedylockboxes", "GreedyLockboxes", "1.0.0")]
+    public class NoMoreTripleQuestion : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            On.RoR2.PurchaseInteraction.GetInteractability += PurchaseInteraction_GetInteractability;
+            On.RoR2.Interactor.PerformInteraction += Interactor_PerformInteraction;
+        }
+
+        private void Interactor_PerformInteraction(On.RoR2.Interactor.orig_PerformInteraction orig, Interactor self, GameObject interactableObject)
+        {
+            CharacterBody buddy = self.GetComponent<CharacterBody>();
+            var purchInter = interactableObject.GetComponent<PurchaseInteraction>();
+            if (purchInter && purchInter.displayNameToken.ToLower() == "lockbox_name")
+            {
+                Debug.Log(purchInter.displayNameToken.ToLower());
+                if (purchInter.costType == CostTypeIndex.None) {
+                    if (buddy && buddy.inventory && buddy.inventory.GetItemCount(ItemIndex.TreasureCache) == 0)
+                    {
+                        return;
+                    }
+                }
+            }
+        orig(self, interactableObject);
+        }
+
+        private Interactability PurchaseInteraction_GetInteractability(On.RoR2.PurchaseInteraction.orig_GetInteractability orig, PurchaseInteraction self, Interactor activator)
+        {
+            
+            CharacterBody buddy = activator.GetComponent<CharacterBody>();
+            if (self.costType == CostTypeIndex.None) {
+                if (self.displayNameToken.ToLower() == "lockbox_name")
+                {
+                    if (buddy && buddy.inventory && buddy.inventory.GetItemCount(ItemIndex.TreasureCache) == 0)
+                    {
+                        return Interactability.Disabled;
+                    }
+                }
+            }
+            return orig(self,activator);
+        }
+    }
+}
---
+++ GreedyLockboxes/README.md
@@ -0,0 +1,12 @@
+﻿People without a Rusty Key can no longer open Rusty Lockboxes.
+
+Mod made on request. I do not think this mod promotes an healthy enviroment, so it will not see a thunderstore release.
+
+### Installation: 
+Place the dll in your Bepinex/Plugins folder.
+
+## Multiplayer:
+Only Host needs it.
+
+## Changelog:
+- 1.0.0 Release!
