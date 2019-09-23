d0c2d9045e967f4803638a6114fed5039693d7bf
harbingerofme <harbingerofme@gmail.com>
2019-09-23 18:40:13 +0200

    NoForwardSaw

---
+++ NoForwardSaw/NoForwardSaw.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+
+/*
+    Code By Guido "Harb". 
+     */
+
+namespace GreedyLockbox
+{
+    [BepInPlugin("com.harbingerofme.noforwardsaw", "NoForwardSaw", "1.0.0")]
+    public class NoMoreTripleQuestion : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            On.EntityStates.Toolbot.FireBuzzsaw.OnEnter += FireBuzzsaw_OnEnter;
+        }
+
+        private void FireBuzzsaw_OnEnter(On.EntityStates.Toolbot.FireBuzzsaw.orig_OnEnter orig, EntityStates.Toolbot.FireBuzzsaw self)
+        {
+            orig(self);
+            EntityStates.Toolbot.FireBuzzsaw.selfForceMagnitude = 0;
+        }
+    }
+}
---
+++ NoForwardSaw/README.md
@@ -0,0 +1,12 @@
+﻿Disables the forward movement on MUL-T's saw.
+
+Mod made on request. 
+
+### Installation: 
+Place the dll in your Bepinex/Plugins folder.
+
+## Multiplayer:
+Only Host needs it.
+
+## Changelog:
+- 1.0.0 Release!
