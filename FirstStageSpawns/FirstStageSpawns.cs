c6bc1a0bdd703f314dee43728089bfd7ee6ae43d
harbingerofme <harbingerofme@gmail.com>
2019-09-25 19:42:39 +0200

    FirstStageSpawns

---
+++ FirstStageSpawns/FirstStageSpawns.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+/*
+    Code By Guido "Harb". 
+     */
+
+namespace FirstStageSpawns
+{
+    [BepInPlugin("com.harbingerofme.firststagespawns", "FirstStageSpawns", "1.0.0")]
+    public class FirstStageSpawns : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            On.RoR2.SceneDirector.PopulateScene += SceneDirector_PopulateScene;
+        }
+
+        private void SceneDirector_PopulateScene(On.RoR2.SceneDirector.orig_PopulateScene orig, RoR2.SceneDirector self)
+        {
+            if(RoR2.Run.instance.stageClearCount == 0)
+            {
+                RoR2.Run.instance.stageClearCount = 1;
+                float gold = self.expRewardCoefficient;
+                self.expRewardCoefficient *= 2;
+                orig(self);
+                self.expRewardCoefficient = gold;
+                RoR2.Run.instance.stageClearCount = 0;
+            }
+            else
+            {
+                orig(self);
+            }
+        }
+    }
+}
---
+++ FirstStageSpawns/README.md
@@ -0,0 +1,10 @@
+﻿Spawns enemies on the first stage. They know where you live too. Get going quicker.
+
+### Installation: 
+Place the dll in your Bepinex/Plugins folder.
+
+### Multiplayer:
+Not tested. Let me know if you do test it.
+
+## Changelog:
+- 1.0.0 Release!
