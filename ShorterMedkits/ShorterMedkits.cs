e52caae82b8e6a4e953bd2a07fa3385e5782e7b3
harbingerofme <harbingerofme@gmail.com>
2019-10-01 10:25:31 +0200

    Harbtweks and buffTime changed to 0.9

--- ShorterMedkits/ShorterMedkits.cs
+++ ShorterMedkits/ShorterMedkits.cs
@@ -7,20 +7,21 @@ using BepInEx.Configuration;
 
 namespace ShorterMedkits
 {
-    [BepInPlugin("com.harbingerofme.shortermedkits", "shortermedkits", "1.0.0")]
+    [BepInPlugin("com.harbingerofme.shortermedkits", "shortermedkits", "1.0.1")]
+    [BepInIncompatibility("com.harbingerofme.harbtweaks")]
     public class ShorterMedkits : BaseUnityPlugin
     {
-        public static ConfigWrapper<float> NewTime { get; set; }
+        public static ConfigEntry<float> NewTime { get; set; }
 
         public void Awake()
         {
-            NewTime = Config.Wrap<float>("", "time", "The new time for the medkits. In multiplayer, this is fixed to 0.55 seconds to prevent desyncs from different configs.", 0.55f);
+            NewTime = Config.AddSetting("", "time", 0.9f , new ConfigDescription("The new time for the medkits. In multiplayer, this is fixed to 0.9 seconds to prevent desyncs from different configs.", new AcceptableValueRange<float>(1f/60f,2.2f)));
             On.RoR2.CharacterBody.AddTimedBuff += (orig,self,buffType,duration)=>
                 {
                     if (buffType == RoR2.BuffIndex.MedkitHeal)
                     {
                         if (RoR2.Run.instance.participatingPlayerCount > 1)
-                            duration *= 0.5f;
+                            duration = 0.9f;
                         else
                             duration = NewTime.Value;
                     }
