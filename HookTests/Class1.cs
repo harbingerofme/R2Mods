6cb38be55fc27c89cd981f2b1dfd352e2b281d75
harbingerofme <harbingerofme@gmail.com>
2019-10-03 12:05:59 +0200

    VS stuff

---
+++ HookTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using UnityEngine.core;
+
+namespace HookTests
+{
+    [BepInPlugin("com.harbingerofme.HarbCrate", "hookTests", "0.0.0")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+
+        }
+    }
+}
