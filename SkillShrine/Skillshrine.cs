c59cd7e3727355b1c73402c3bb880739f5a8bd77
harbingerofme <harbingerofme@gmail.com>
2019-10-05 11:31:07 +0200

    Start of some skill related mod

---
+++ SkillShrine/Skillshrine.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using RoR2;
+using System.Collections.Generic;
+using R2API.Utils;
+using RoR2.Skills;
+
+namespace Shrine_of_Skill
+{
+    [BepInPlugin("com.harbingerofme.skillshrine", "skillshrine", "0.0.0")]
+    public class SkillShrineMod : BaseUnityPlugin
+    {
+        public List<GenericSkill> skillList;
+        public void Start()
+        {
+            skillList = new List<GenericSkill>();
+            var a = RoR2.Skills.SkillCatalog.allSkillDefs;
+            a.ForEachTry((
+                skillDef) =>
+            {
+                Logger.LogInfo(skillDef.skillName);
+            });
+            
+        }
+    }
+}
