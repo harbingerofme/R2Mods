6dcdc64fea9b026c7e74f3e875d0f305117cf376
harbingerofme <harbingerofme@gmail.com>
2019-10-28 20:18:41 +0100

    NFS is now a tweak

--- HarbTweaks/Modules/NoForwardSaw.cs
+++ HarbTweaks/Modules/NoForwardSaw.cs
@@ -8,43 +8,31 @@ using System;
 
 namespace HarbTweaks
 {
-    
-    public class NoForwardSaw 
+    [HarbTweak(TweakName, DefaultEnabled, Description)]
+    internal sealed class NoForwardSaw : Tweak
     {
 
-        private bool Enabled { get { return enabled.Value; } }
-        private readonly ConfigEntry<bool> enabled;
-        private bool prevEnabled = false;
+        private const string TweakName = "No Forward Saw";
+        private const bool DefaultEnabled = false;
+        private const string Description = "Makes MUL-T's Powersaw no longer push you forward.";
 
-        private readonly ConfigEntry<float> newForce;
+        private ConfigEntry<float> newForce;
 
-        private readonly string name = "No Forward Saw";
+        public NoForwardSaw(ConfigFile config, string name, bool defaultEnabled, string description) : base(config, name, defaultEnabled, description)
+        { }
 
-        public NoForwardSaw()
+        protected override void MakeConfig()
         {
-            enabled = HarbTweaks.Instance.AddConfig(name, "Enabled", false, "Makes MUL-T's Powersaw no longer push you forward.", ReloadHook);
-            newForce = HarbTweaks.Instance.AddConfig(name, "New force to apply", 0f, "Force applied to MULT when the saw connects.", ReloadHook);
-            if (Enabled)
-                MakeHook();
+            newForce = AddConfig("New force to apply", 0f, "Force applied to MULT when the saw connects.");
         }
 
-        public void ReloadHook(object o, EventArgs args)
-        {
-            if (prevEnabled)
-                RemoveHook();
-            if (Enabled)
-                MakeHook();
-        }
-
-        public void MakeHook()
+        protected override void Hook()
         {
             On.EntityStates.Toolbot.FireBuzzsaw.OnEnter += FireBuzzsaw_OnEnter;
-            prevEnabled = true;
         }
-        public void RemoveHook()
+        protected override void UnHook()
         {
             On.EntityStates.Toolbot.FireBuzzsaw.OnEnter -= FireBuzzsaw_OnEnter;
-            prevEnabled = false;
         }
 
         private void FireBuzzsaw_OnEnter(On.EntityStates.Toolbot.FireBuzzsaw.orig_OnEnter orig, EntityStates.Toolbot.FireBuzzsaw self)
