8ae44d6b4e2cbaff01669b5e5564eb393a9d9dec
harbingerofme <harbingerofme@gmail.com>
2019-11-07 06:30:27 +0100

    emptyplugin for easy duplication

---
+++ emptyPlugin/Class1.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+
+
+namespace testPlugin
+{
+    [PluginMetadata(GUID,NAME,VERSION)]
+    public class emptyPlugin : BaseUnityPlugin
+    {
+        public const string
+            NAME = "emptyPlugin",
+            GUID = "test.harbingerofme." + NAME,
+            VERSION = "0.0.0";
+
+        public void Awake()
+        {
+
+        }
+    }
+}
