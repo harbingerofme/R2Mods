5f1356ae2e8c75d3729500672c775a6214a16fed
Harbingerofme <harbingerofme@gmail.com>
2019-11-09 15:23:12 +0100

    made it an r2api test plugin

--- Deluge/Class1.cs
+++ Deluge/Class1.cs
@@ -18,48 +18,18 @@ namespace Deluge
 
 
         private DifficultyDef DelugeDef;
+        private DifficultyDef RiskOfRain2DiscordDef;
 
         private Deluge()
         {
             DelugeDef = new DifficultyDef(3.5f, "DIFFICULTY_DELUGE_NAME", "Textures/DifficultyIcons/texDifficultyHardIcon", "DIFFICULTY_DELUGE_DESCRIPTION", new Color(0.6f, 1, 0.2f));
+            RiskOfRain2DiscordDef = new DifficultyDef(0.5f, "DIFFICULTY_RISKOFRAIN2DISCORD_NAME", "Textures/DifficultyIcons/texDifficultyHardIcon", "DIFFICULTY_RISKOFRAIN2DISCORD_DESCRIPTION", new Color(0.6f, 0.4f, 1f));
         }
 
         public void Awake()
         {
-            On.RoR2.DifficultyCatalog.GetDifficultyDef += DifficultyCatalog_GetDifficultyDef;
-            On.RoR2.RuleDef.FromDifficulty += RuleDef_FromDifficulty;
-        }
-
-
-        public void Start()
-        {
-            DifficultyDef[] premod = (DifficultyDef[]) typeof(DifficultyCatalog).GetFieldCached("difficultyDefs").GetValue(null);//GetFieldValu<>("difficultyDefs");
-            DifficultyDef[] newDefs = new DifficultyDef[premod.Length + 1];
-            premod.CopyTo(newDefs,0);
-            newDefs[premod.Length] = DelugeDef;
-            DiffIndexOverride = premod.Length;
-            typeof(DifficultyCatalog).GetFieldCached("difficultyDefs").SetValue(null, newDefs);
-        }
-
-        private DifficultyDef DifficultyCatalog_GetDifficultyDef(On.RoR2.DifficultyCatalog.orig_GetDifficultyDef orig, DifficultyIndex difficultyIndex)
-        {
-            Debug.Log(difficultyIndex);
-            if((int) difficultyIndex == DiffIndexOverride)
-            {
-                return DelugeDef;
-            }
-            return orig(difficultyIndex);
-        }
-        private RuleDef RuleDef_FromDifficulty(On.RoR2.RuleDef.orig_FromDifficulty orig)
-        {
-            RuleDef def = orig();
-            RuleChoiceDef choice = def.AddChoice("Deluge", null, false);
-            choice.spritePath = DelugeDef.iconPath;
-            choice.tooltipNameToken = DelugeDef.nameToken;
-            choice.tooltipNameColor = DelugeDef.color;
-            choice.tooltipBodyToken = DelugeDef.descriptionToken;
-            choice.difficultyIndex = (DifficultyIndex)DiffIndexOverride;
-            return def;
+            R2API.DifficultyAPI.AddDifficulty(DelugeDef);
+            R2API.DifficultyAPI.AddDifficulty(RiskOfRain2DiscordDef);
         }
 
     }
