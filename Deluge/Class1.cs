9000b08f2b5be8dc2526d3ae9d9d9c0a8e49667b
harbingerofme <harbingerofme@gmail.com>
2019-11-11 20:51:05 +0100

    Add the everything

--- Deluge/Class1.cs
+++ Deluge/Class1.cs
@@ -1,11 +1,17 @@
 ﻿using BepInEx;
 using RoR2;
-using System.Reflection;
-using R2API.Utils;
 using UnityEngine;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using R2API.Utils;
+using System;
+using EliteDef = RoR2.CombatDirector.EliteTierDef;
 
 namespace Deluge
 {
+
+    [BepInDependency(R2API.R2API.PluginGUID,BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.jarlyk.eso", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInPlugin(GUID,NAME,VERSION)]
     public class Deluge : BaseUnityPlugin
     {
@@ -14,23 +20,201 @@ namespace Deluge
             GUID = "test.harbingerofme." + NAME,
             VERSION = "0.0.1";
 
-        public static int DiffIndexOverride = 4;
+        private readonly Color DelugeColor;
+        private readonly DifficultyDef DelugeDef;
+        private DifficultyIndex DelugeIndex;
+        private bool HooksApplied = false;
 
 
-        private DifficultyDef DelugeDef;
-        private DifficultyDef RiskOfRain2DiscordDef;
+        private bool ESOenabled = false;
+        private float[] vanillaEliteMultipliers;
+        private EliteDef[] CombatDirectorTierDefs;
+        private float DelugeEliteModifier = 0.8f;
 
         private Deluge()
         {
-            DelugeDef = new DifficultyDef(2.5f, "DIFFICULTY_DELUGE_NAME", "Textures/AchievementIcons/texLoaderClearGameMonsoonIcon", "DIFFICULTY_DELUGE_DESCRIPTION", new Color(0.1f, 1, 0.2f));
-            RiskOfRain2DiscordDef = new DifficultyDef(0.5f, "DIFFICULTY_RISKOFRAIN2DISCORD_NAME", "Textures/AchievementIcons/texCommandoClearGameMonsoonIcon", "DIFFICULTY_RISKOFRAIN2DISCORD_DESCRIPTION", new Color(0.6f, 0.4f, 1f));
+            DelugeColor = new Color(0.61f, 0.07f, 0.93f);
+            DelugeDef = new DifficultyDef(
+                            3.5f,
+                            "DIFFICULTY_DELUGE_NAME",
+                            "Textures/ItemIcons/texdevilhornsicon",
+                            "DIFFICULTY_DELUGE_DESCRIPTION",
+                            DelugeColor
+                            );
         }
 
         public void Awake()
         {
-            R2API.DifficultyAPI.AddDifficulty(DelugeDef);
-            R2API.DifficultyAPI.AddDifficulty(RiskOfRain2DiscordDef);
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.jarlyk.eso")){
+                ESOenabled = true;
+                Logger.LogWarning("ESO detected: Delegating Elite modifications to them. Future support planned.");
+            }
+
+            CombatDirectorTierDefs = (EliteDef[]) typeof(CombatDirector).GetFieldCached("eliteTiers").GetValue(null);
+            vanillaEliteMultipliers = new float[CombatDirectorTierDefs.Length];
+
+            DelugeIndex = R2API.DifficultyAPI.AddDifficulty(DelugeDef);
+
+            R2API.AssetPlus.Languages.AddToken("DIFFICULTY_DELUGE_NAME", "Deluge");
+            string description = "For the players who want even more. N'Kuhana herself watches over you.<style=cStack>\n";
+            description = string.Join("\n",
+                description,
+                $">Difficulty Scaling: +{DelugeDef.scalingValue*50-100}%",
+                ">Player Health Regeneration: -50%",
+                ">Monster Health Regeneration: +1% of MaxHP per second",
+                ">Oneshot Protection: Also applies to monsters",
+                ">Oneshot Protection: Hits do a maximum of 99%",
+                ">Teleporter: Enemies don't stop spawning after charge finish",
+                $">Elites: {(1- DelugeEliteModifier)*100}% cheaper."
+                );
+            description += "</style>";
+            R2API.AssetPlus.Languages.AddToken("DIFFICULTY_DELUGE_DESCRIPTION", description);
+
+
+            Run.onRunStartGlobal += Run_onRunStartGlobal;
+            Run.onRunDestroyGlobal += Run_onRunDestroyGlobal;
+            
         }
 
+
+
+        private void Run_onRunStartGlobal(Run run)
+        {
+            ChatMessage.SendColored("A storm is brewing.", DelugeColor);
+            if (run.selectedDifficulty == DelugeIndex && HooksApplied == false)
+            {
+                HooksApplied = true;
+                IL.RoR2.HealthComponent.TakeDamage += ChangeOSP;
+                IL.RoR2.CharacterBody.RecalculateStats += AdjustRegen;
+                IL.RoR2.TeleporterInteraction.StateFixedUpdate += NoSafetyAfterFinishCharging;
+                TeleporterInteraction.onTeleporterFinishGlobal += MakeSureBonusDirectorDiesOnStageFinish;
+                if (!ESOenabled)
+                {
+                    for (int i = 0; i < CombatDirectorTierDefs.Length; i++)
+                    {
+                        EliteDef tierDef = CombatDirectorTierDefs[i];
+                        vanillaEliteMultipliers[i] = tierDef.costMultiplier;
+                        tierDef.costMultiplier *= DelugeEliteModifier;
+                    }
+                }
+            }
+        }
+
+
+
+        private void Run_onRunDestroyGlobal(Run obj)
+        {
+            if (HooksApplied)
+            {
+                IL.RoR2.HealthComponent.TakeDamage -= ChangeOSP;
+                IL.RoR2.CharacterBody.RecalculateStats -= AdjustRegen;
+                IL.RoR2.TeleporterInteraction.StateFixedUpdate -= NoSafetyAfterFinishCharging;
+                TeleporterInteraction.onTeleporterFinishGlobal -= MakeSureBonusDirectorDiesOnStageFinish;
+                if (!ESOenabled)
+                {
+                    for (int i = 0; i < CombatDirectorTierDefs.Length; i++)
+                    {
+                        EliteDef tierDef = CombatDirectorTierDefs[i];
+                        tierDef.costMultiplier = vanillaEliteMultipliers[i];
+                    }
+                }
+                HooksApplied = false;
+            }
+        }
+
+        private void MakeSureBonusDirectorDiesOnStageFinish(TeleporterInteraction obj)
+        {
+            if(obj.bonusDirector && obj.bonusDirector.enabled)
+            {
+                obj.bonusDirector.enabled = false;
+            }
+        }
+
+        private void NoSafetyAfterFinishCharging(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            c.GotoNext(MoveType.After,
+                x => x.MatchLdfld<TeleporterInteraction>("bonusDirector")
+                );
+            c.GotoNext(MoveType.Before,
+                x => x.MatchLdfld<TeleporterInteraction>("bonusDirector"),
+                x => x.MatchLdcI4(0)
+                );
+            c.Index +=1;
+            c.RemoveRange(2);
+            c.EmitDelegate<Action<CombatDirector>>((director) =>
+            {
+                director.expRewardCoefficient = 0f;
+            }
+            );
+        }
+
+        private void AdjustRegen(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            int monsoonPlayerHelperCountIndex = 25;
+            c.GotoNext(
+                x => x.MatchLdcI4((int)ItemIndex.MonsoonPlayerHelper),
+                x => x.MatchCallvirt<Inventory>("GetItemCount"),
+                x => x.MatchStloc(out monsoonPlayerHelperCountIndex)
+                );
+            int regenMultiIndex = 37;
+            c.GotoNext(MoveType.Before,
+                x => x.MatchStloc(out regenMultiIndex),
+                x => x.MatchLdloc(monsoonPlayerHelperCountIndex)
+                );
+            c.Index += 1;
+            c.Emit(OpCodes.Ldarg_0);
+            c.Emit(OpCodes.Ldloc, regenMultiIndex);
+            c.EmitDelegate<Func<CharacterBody, float, float>>((self, regenMulti) =>
+            {
+                if (self.isPlayerControlled)
+                {
+                    regenMulti -= 0.5f;
+                    Debug.Log("Reduced RegenMulti to: " + regenMulti);
+                }
+                return regenMulti;
+            });
+            c.Emit(OpCodes.Stloc, regenMultiIndex);
+            int regenIndex = 36;
+            c.GotoPrev(MoveType.Before,
+                x => x.MatchStloc(out regenIndex),
+                x => x.MatchLdcR4(1),
+                x => x.MatchStloc(regenMultiIndex)
+                ); ;
+            c.Index += 1;
+            c.Emit(OpCodes.Ldarg_0);
+            c.Emit(OpCodes.Ldloc, regenIndex);
+            c.EmitDelegate<Func<CharacterBody, float, float>>((self, regen) =>
+            {
+                if (self.teamComponent.teamIndex == TeamIndex.Monster){
+                    regen += self.maxHealth * 0.01f;
+                }
+                return regen;
+            });
+            c.Emit(OpCodes.Stloc, regenIndex);
+        }
+        private void ChangeOSP(ILContext il)
+        {
+            try
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(MoveType.Before,
+                    x => x.MatchCallvirt<HealthComponent>("get_hasOneshotProtection")
+                    );
+                c.RemoveRange(2);
+                c.Emit(OpCodes.Pop);
+                c.GotoNext(MoveType.Before,
+                        x => x.MatchLdcR4(0.9f)
+                        );
+                c.Remove();
+                c.Emit(OpCodes.Ldc_R4,0.99f);
+                    }
+            catch (Exception e)
+            {
+                Logger.LogWarning("Couldn't modify OneShotProtection. Maybe you have a mod interfering. Game might act weird.");
+                Logger.LogError(e);
+            }
+        }
     }
 }
