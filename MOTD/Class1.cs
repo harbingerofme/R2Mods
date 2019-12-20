5fb0fbd308c72d0e057eed133696d520afd91be8
Harbingerofme <harbingerofme@gmail.com>
2019-12-21 00:18:25 +0100

    configurable now

--- MOTD/Class1.cs
+++ MOTD/Class1.cs
@@ -4,6 +4,8 @@ using UnityEngine.Networking;
 using MonoMod.Cil;
 using static MonoMod.Cil.RuntimeILReferenceBag.FastDelegateInvokers;
 using Mono.Cecil.Cil;
+using System.Collections.Generic;
+using BepInEx.Configuration;
 
 namespace MotD
 {
@@ -15,12 +17,29 @@ namespace MotD
             GUID = "com.harbingerofme." + NAME,
             VERSION = "0.0.1";
 
+        private string modList = "";
 
+        ConfigEntry<string> motd;
 
         public void Awake()
         {
             On.RoR2.RoR2Application.UnitySystemConsoleRedirector.Redirect += orig => { };
             IL.RoR2.Networking.GameNetworkManager.OnServerAddPlayerInternal += GameNetworkManager_OnServerAddPlayerInternal1;
+            motd = Config.Bind<string>(new ConfigDefinition("", ""), "This server runs: %MODLIST%", new ConfigDescription("You can use the following tokens: %STEAM%, %MODLIST%."));
+        }
+
+        private string GetModList()
+        {
+            if (modList == "")
+            {
+                List<string> nameList = new List<string>();
+                foreach (KeyValuePair<string, PluginInfo> entry in BepInEx.Bootstrap.Chainloader.PluginInfos)
+                {
+                    nameList.Add($"[{entry.Value.Metadata.Name}]");
+                }
+                modList = string.Join(",", nameList);
+            }
+            return modList;
         }
 
         private void GameNetworkManager_OnServerAddPlayerInternal1(ILContext il)
@@ -33,10 +52,25 @@ namespace MotD
             c.Emit(OpCodes.Ldarg_1);
             c.EmitDelegate<Action<NetworkConnection>>((conn) =>
             {
-                var steam = RoR2.Networking.ServerAuthManager.FindAuthData(conn).steamId;
-                var message = new Chat.SimpleChatMessage() { baseToken = "{0}", paramTokens = new[] { $"Hello user with steamid: {steam}" } };
+               
+                var message = new Chat.SimpleChatMessage() { baseToken = "{0}", paramTokens = new[] { GenerateMotDFormatted(conn) } };
                 MotD.SendPrivateMessage(message, conn);
             });
+        }
+
+        private string GenerateMotDFormatted(NetworkConnection conn)
+        {
+            string message = motd.Value;
+            if (message.Contains("%STEAM%"))
+            {
+                var steam = RoR2.Networking.ServerAuthManager.FindAuthData(conn).steamId;
+                message = message.Replace("%STEAM%", steam.ToString());
+            }
+            if (message.Contains("%MODLIST%"))
+            {
+                message = message.Replace("%MODLIST%", GetModList());
+            }
+            return message;
         }
     }
 
