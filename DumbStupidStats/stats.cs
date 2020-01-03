fdf1de9935edc82a980d2fec5a16e6f88b7a9541
Harbingerofme <harbingerofme@gmail.com>
2020-01-03 17:32:18 +0100

    Healed stat

---
+++ DumbStupidStats/stats.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using RoR2;
+using RoR2.Stats;
+
+namespace DumbStupidStats
+{
+
+    [DumbStatDef]
+    class Healed : DumbStat
+    {
+        private const string token = "dss_healed";
+        private const string fullText = "Amount Healed";
+        private const StatRecordType recordType = StatRecordType.Sum;
+        private const StatDataType dataType = StatDataType.Double;
+        public Healed()
+        {
+            Definition = StatDef.Register(token, recordType, dataType, 0, null);
+            FullText = fullText;
+            HealthComponent.onCharacterHealServer += HealthComponent_onCharacterHealServer;
+        }
+
+        private void HealthComponent_onCharacterHealServer(HealthComponent healthComponent, float amount)
+        {
+            var body = healthComponent.body;
+            if (body.isPlayerControlled)
+            {
+                PlayerStatsComponent.FindBodyStatSheet(body).PushStatValue(Definition, Convert.ToDouble(amount));
+            }
+        }
+    }
+}
