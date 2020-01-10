b277fde5b7b82a25d4b32e0774b32f44a8a07d4e
Harbingerofme <github@harbingerofme.info>
2020-01-10 15:17:13 +0100

    Remove duplicate stats
    They are in TimeStats.cs

--- DumbStupidStats/Stats/stats.cs
+++ DumbStupidStats/Stats/stats.cs
@@ -64,60 +64,6 @@ namespace DumbStupidStats.Stats
     }
 
     [DumbStatDef]
-    class AbsoluteTime : DumbStat
-    {
-        private const string token = "dss_AbsoluteTime";
-        private const string fullText = "Real Time";
-        private DateTime time;
-
-        public AbsoluteTime(){
-            Definition = StatDef.Register(token, StatRecordType.Sum, StatDataType.Double, 0, new StatDef.DisplayValueFormatterDelegate(StatDef.TimeMMSSDisplayValueFormatter));
-            FullText = fullText;
-
-        }
-
-        public override void Activate()
-        {
-            time = DateTime.Now;
-        }
-
-        public override void DeActivate()
-        {
-            var newNow = DateTime.Now;
-            var difference = newNow.Subtract(time);
-            foreach(var pcmc in PlayerCharacterMasterController.instances)
-            {
-                if (pcmc & pcmc.master && pcmc.master.GetBody())
-                    PlayerStatsComponent.FindBodyStatSheet(pcmc.master.GetBody()).PushStatValue(Definition,(float) difference.TotalSeconds);
-            }
-        }
-    }
-    [DumbStatDef]
-    class WastedTime : DumbStat{
-        private const string token = "dss_WastedTime";
-        private const string fullText = "Paused Time";
-        public WastedTime(){
-            Definition = StatDef.Register(token, StatRecordType.Sum, StatDataType.Double, 0, new StatDef.DisplayValueFormatterDelegate(StatDef.TimeMMSSDisplayValueFormatter));
-            FullText = fullText;
-        }
-
-        public override void Activate()
-        {
-            On.RoR2.Stats.PlayerStatsComponent.ServerFixedUpdate += PlayerStatsComponent_ServerFixedUpdate;
-        }
-
-        public override void DeActivate()
-        {
-            On.RoR2.Stats.PlayerStatsComponent.ServerFixedUpdate -= PlayerStatsComponent_ServerFixedUpdate;
-        }
-
-        private void PlayerStatsComponent_ServerFixedUpdate(On.RoR2.Stats.PlayerStatsComponent.orig_ServerFixedUpdate orig, PlayerStatsComponent self){
-            orig(self);
-            if (Run.instance && Run.instance.isRunStopwatchPaused && self.characterMaster && self.characterMaster.GetBody())
-                PlayerStatsComponent.FindBodyStatSheet(self.characterMaster.GetBody()).PushStatValue(Definition, UnityEngine.Time.fixedDeltaTime);
-        }
-    }
-    [DumbStatDef]
     class DamageBlocked : DumbStat{
         private const string token = "dss_DamageBlocked";
         private const string fullText = "Damage instances blocked";
