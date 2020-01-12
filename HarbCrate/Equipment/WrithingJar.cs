72001562cb4771da421bb305cd1cb380f3c41220
Harbingerofme <github@harbingerofme.info>
2020-01-12 23:17:40 +0100

    Update to new format

--- HarbCrate/Equipment/WrithingJar.cs
+++ HarbCrate/Equipment/WrithingJar.cs
@@ -1,32 +1,47 @@
-﻿using RoR2;
+﻿using R2API;
+using RoR2;
 using UnityEngine;
 
 namespace HarbCrate.Equipment
 {
-    public sealed class WrithingJar : HarbEquipment
+    [Equipment]
+    public sealed class WrithingJar : IPickup, IEquip
     {
-        public new static readonly float Cooldown = 120;
-        public new static readonly string Name = "The Writhing Jar";
-
-        public new static CustomEquipment Build() {
-            EquipmentDef myDef = new EquipmentDef
-            {
-                cooldown = Cooldown,
-                pickupModelPath = "Prefabs/PickupModels/PickupWilloWisp",
-                pickupIconPath = "Textures/ItemIcons/texWilloWispIcon",
-                nameToken = Name,
-                pickupToken = "Inside of you there's two worms. One is a friend, the other a monster. You are a monster.",
-                descriptionToken = "Summons a friendly Magma Worm and a HOSTILE Overloading Magma Worm.",
-                canDrop = true,
-                isLunar = true,
-                colorIndex = ColorCatalog.ColorIndex.LunarItem,
-                enigmaCompatible = false
-            };
-            return new CustomEquipment(myDef, null, null, null);
-        }
-        public new static bool Effect(EquipmentSlot equipmentSlot)
+        public TokenValue Name => new TokenValue
         {
-            
+            Token = "HC_WORMJAR",
+            Value = "The Writhing Jar"
+        };
+
+        public TokenValue Description => new TokenValue
+        {
+            Token = "HC_WORMJAR_DESC",
+            Value =
+                "Summons a <style=cHealing>friendly</style> Magma Worm and a <style=cDeath>HOSTILE</cstyle> Overloading Magma Worm."
+        };
+
+        public TokenValue PickupText => new TokenValue
+        {
+            Token = "HC_WORMJAR_DESC",
+            Value = "Inside of you there's two worms. One is a friend, the other a monster. You are a <color=blue>monster</color>."
+        };
+
+        public string AssetPath => "";
+        public string SpritePath => "";
+
+        public EquipmentDef Definition { get; set; }
+        public CustomEquipment CustomDef { get; set; }
+        public float Cooldown => 120f;
+        public bool IsLunar => true;
+        public bool IsEnigmaCompat => true;
+
+        private const float HostileDMG = 3;
+        private const float HostileHP = 4.7f;
+        private const float AllyDMG = 1.5f;
+        private const float AllyHP = 1;
+
+        public bool Effect(EquipmentSlot equipmentSlot)
+        {
             var transform = equipmentSlot.transform;
             var placementRules = new DirectorPlacementRule
             {
@@ -48,10 +63,11 @@ namespace HarbCrate.Equipment
                 CharacterMaster cm = spawn.GetComponent<CharacterMaster>();
                 if (cm)
                 {
-                    cm.inventory.GiveItem(ItemIndex.BoostDamage, 20);
-                    cm.inventory.GiveItem(ItemIndex.BoostHp, 47);
+                    cm.inventory.GiveItem(ItemIndex.BoostDamage, getItemCountFromMultiplier(HostileDMG));
+                    cm.inventory.GiveItem(ItemIndex.BoostHp, getItemCountFromMultiplier((HostileHP)));
                 }
             }
+
             var friendCard = Resources.Load<SpawnCard>("SpawnCards/CharacterSpawnCards/cscMagmaWorm");
             var friendRequest = new DirectorSpawnRequest(friendCard, placementRules, RoR2Application.rng)
             {
@@ -61,9 +77,27 @@ namespace HarbCrate.Equipment
             };
             var spawn2 = DirectorCore.instance.TrySpawnObject(friendRequest);
             spawn2.transform.TransformDirection(0, 100, 0);
+            if (spawn2)
+            {
+                CharacterMaster cm = spawn2.GetComponent<CharacterMaster>();
+                if (cm)
+                {
+                    cm.inventory.GiveItem(ItemIndex.BoostDamage, getItemCountFromMultiplier(AllyDMG));
+                    cm.inventory.GiveItem(ItemIndex.BoostHp, getItemCountFromMultiplier((AllyHP)));
+                }   
+            }
             if (spawn || spawn2)
                 return true;
             return false;
         }
+
+
+        private int getItemCountFromMultiplier(float multiplier)
+        {
+            if (multiplier <= 1)
+                return 0;
+            return Mathf.CeilToInt((multiplier - 1) * 10);
+        }
+
     }
 }
