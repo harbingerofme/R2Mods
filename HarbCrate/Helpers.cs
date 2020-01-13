3e5238b7ff46d6a66a78f4ffa774b78fd61239f8
Harbingerofme <github@harbingerofme.info>
2020-01-13 16:04:55 +0100

    Make self defining properties

--- HarbCrate/Helpers.cs
+++ HarbCrate/Helpers.cs
@@ -39,8 +39,10 @@ namespace HarbCrate
         public TokenValue Name;
         public TokenValue Description;
         public TokenValue PickupText;
+        public TokenValue Lore;
         public string AssetPath;
         public string SpritePath;
+        public ItemDisplayRule[] DisplayRules = null;
 
         public abstract void Hook();
     }
@@ -51,20 +53,96 @@ namespace HarbCrate
         { }
 
         public ItemTier Tier;
-        public ItemDef Definition;
-        public CustomItem CustomDef;
+
+        private ItemDef _definiton = null;
+        public ItemDef Definition
+        {
+            get
+            {
+                if (_definiton == null)
+                {
+                    _definiton = new ItemDef
+                    {
+                        name = Name.Value,
+                        canRemove = true,
+                        descriptionToken = Description.Token,
+                        hidden = (Tier == (Tier) - 1),
+                        loreToken = Lore.Token,
+                        nameToken = Name.Token,
+                        itemIndex = ItemIndex.None,
+                        pickupIconPath = SpritePath,
+                        pickupModelPath = AssetPath,
+                        pickupToken = PickupText.Token,
+                        unlockableName = ""
+                    };
+                }
+                return _definiton;
+            }
+        }
+        private CustomItem customDef;
+
+        public CustomItem CustomDef
+        {
+            get
+            {
+                if (customDef == null)
+                {
+                    customDef = new CustomItem(Definition,DisplayRules);
+                }
+                return customDef;
+            }
+        }
     }
 
     internal  abstract  class Equip:  Pickup
     {
         public Equip() : base()
         { }
-
-        public EquipmentDef Definition;
-        public CustomEquipment CustomDef;
+        
         public float Cooldown;
         public bool IsLunar;
         public bool IsEnigmaCompat;
         public abstract bool Effect(EquipmentSlot equipmentSlot);
+
+        private CustomEquipment customDef;
+
+        public CustomEquipment CustomDef
+        {
+            get
+            {
+                if (customDef == null)
+                {
+                    customDef = new CustomEquipment(Definition,DisplayRules);
+                }
+                return customDef;
+            }
+        }
+        
+        
+        private EquipmentDef definition;
+        public EquipmentDef Definition
+        {
+            get
+            {
+                if (definition == null)
+                {
+                    definition = new EquipmentDef()
+                    {
+                        name = Name.Value,
+                        descriptionToken = Description.Token,
+                        loreToken = Lore.Token,
+                        nameToken = Name.Token,
+                        pickupIconPath = SpritePath,
+                        pickupModelPath = AssetPath,
+                        pickupToken = PickupText.Token,
+                        cooldown = Cooldown,
+                        isLunar = IsLunar,
+                        enigmaCompatible= IsEnigmaCompat,
+                        unlockableName = ""
+                    };
+                }
+                return definition;
+            }
+        }
     }
 }
