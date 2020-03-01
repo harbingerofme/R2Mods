e3d8e24041f1525ae51c8d392898d7bfef8b2647
HarbingerOfMe <harbingerofme@gmail.com>
2020-03-01 01:29:07 +0100

    Add AssetPlusRequester

---
+++ AssetPlusRequester/AssetPlusRequesterPlugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using R2API.Utils;
+
+namespace AssetPlusRequester
+{
+
+    [BepInDependency("com.bepis.r2api")]
+    [R2APISubmoduleDependency(nameof(R2API.AssetPlus))]
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public sealed class AssetPlusRequesterPlugin : BaseUnityPlugin
+    {
+        public const string
+            MODNAME = "AssetPlusRequester",
+            AUTHOR = "Harb",
+            GUID = MODNAME,
+            VERSION = "1.0.0";
+
+    }
+}
