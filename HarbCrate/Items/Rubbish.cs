855c07bf06a0a3cc234dc4441be0192ad368b72c
HarbingerOfMe <harbingerofme@gmail.com>
2020-05-08 17:31:01 +0200

    Fixup Rubbish.

--- HarbCrate/Items/Rubbish.cs
+++ HarbCrate/Items/Rubbish.cs
@@ -24,8 +24,8 @@ namespace HarbCrate.Items
                 Token = "HC_RUBBISH_PICKUP",
                 Value = "Useless. But one man's trash..."
             };
-            AssetPath = "";
-            SpritePath = @"Assets\Resources\textures\itemicons\bg\texRuleMapIsRandom.png";
+            AssetPath = "Prefabs/PickupModels/PickupMystery";
+            SpritePath = "Textures/MiscIcons/texMysteryIcon";
             Tier = ItemTier.Tier1;
             Tags = new ItemTag[]
             {
@@ -35,10 +35,20 @@ namespace HarbCrate.Items
 
         public override void Hook()
         {
-            On.RoR2.CharacterBody.OnInventoryChanged += CharacterBody_OnInventoryChanged;
+            On.RoR2.CharacterBody.OnInventoryChanged += MaybeAwardLoot;
+            On.RoR2.Chat.AddPickupMessage += FixPickupMessage;
         }
 
-        private void CharacterBody_OnInventoryChanged(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self)
+        private void FixPickupMessage(On.RoR2.Chat.orig_AddPickupMessage orig, CharacterBody body, string pickupToken, Color32 pickupColor, uint pickupQuantity)
+        {
+            if(pickupToken == Name.Token && pickupQuantity == 0)
+            {
+                pickupQuantity = 3;
+            }
+            orig(body, pickupToken, pickupColor, pickupQuantity);
+        }
+
+        private void MaybeAwardLoot(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self)
         {
             orig(self);
             var amount = self.inventory.GetItemCount(Definition.itemIndex);
@@ -54,7 +64,15 @@ namespace HarbCrate.Items
                 {
                     loot = Run.instance.treasureRng.NextElementUniform(Run.instance.availableTier2DropList);
                 }
-                PickupDropletController.CreatePickupDroplet(loot, self.corePosition, Vector3.up * 5);
+                if(self.isPlayerControlled)
+                    PickupDropletController.CreatePickupDroplet(loot, self.corePosition, Vector3.up * 5);
+                else
+                {
+                    PickupDef def = PickupCatalog.GetPickupDef(loot);
+                    self.inventory.GiveItem(def.itemIndex);
+                    var lootCount = self.inventory.GetItemCount(def.itemIndex);
+                    Chat.AddPickupMessage(self, def.nameToken, ColorCatalog.GetColor(ItemCatalog.GetItemDef(def.itemIndex).colorIndex), (uint) lootCount);
+                }
             }
         }
     }
